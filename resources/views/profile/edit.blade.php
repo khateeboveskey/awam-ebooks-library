0568b6dfd445e90c963085b5e4f04f2bc5d02155
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2024-12-05 21:27:30 +0300

    translate profile partials

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,31 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-primary-800 dark:text-primary-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
+@section('header')
+    <h2 class="text-xl font-semibold leading-tight text-primary-800 dark:text-primary-200">
+        {{ __('Profile') }}
+    </h2>
+@endsection
+
+@section('content')
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-primary-800 shadow sm:rounded-lg">
+        <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+            <div class="bg-white p-4 shadow dark:bg-primary-800 sm:rounded-lg sm:p-8">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-primary-800 shadow sm:rounded-lg">
+            <div class="bg-white p-4 shadow dark:bg-primary-800 sm:rounded-lg sm:p-8">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-primary-800 shadow sm:rounded-lg">
+            <div class="bg-white p-4 shadow dark:bg-primary-800 sm:rounded-lg sm:p-8">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,11 +1,11 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-primary-900 dark:text-primary-100">
-            {{ __('Profile Information') }}
+            معلومات الملف الشخصي
         </h2>
 
         <p class="mt-1 text-sm text-primary-600 dark:text-primary-400">
-            {{ __("Update your account's profile information and email address.") }}
+            قم بتحديث معلومات ملفك الشخصي وعنوان بريدك الإلكتروني.
         </p>
     </header>
 
@@ -18,29 +18,29 @@
         @method('patch')
 
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="'الاسم'" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
 
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="'البريد الإلكتروني'" />
             <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-primary-800 dark:text-primary-200">
-                        {{ __('Your email address is unverified.') }}
+                        عنوان بريدك الإلكتروني غير مؤكد.
 
                         <button form="send-verification" class="underline text-sm text-primary-600 dark:text-primary-400 hover:text-primary-900 dark:hover:text-primary-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 dark:focus:ring-offset-primary-800">
-                            {{ __('Click here to re-send the verification email.') }}
+                            انقر هنا لإعادة إرسال رسالة التحقق.
                         </button>
                     </p>
 
                     @if (session('status') === 'verification-link-sent')
                         <p class="mt-2 font-medium text-sm text-green-600 dark:text-green-400">
-                            {{ __('A new verification link has been sent to your email address.') }}
+                            تم إرسال رابط تحقق جديد إلى عنوان بريدك الإلكتروني.
                         </p>
                     @endif
                 </div>
@@ -48,7 +48,7 @@
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button>حفظ</x-primary-button>
 
             @if (session('status') === 'profile-updated')
                 <p
@@ -57,7 +57,7 @@
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
                     class="text-sm text-primary-600 dark:text-primary-400"
-                >{{ __('Saved.') }}</p>
+                >تم الحفظ.</p>
             @endif
         </div>
     </form>
